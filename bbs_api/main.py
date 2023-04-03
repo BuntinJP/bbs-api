a7666e0da6cc40dee7e58f65c0fff02cbb223168
Buntin-Synthia <aokitakumi72@gmail.com>
2023-04-03 23:50:59 +0900

    環境構築終了

---
+++ bbs_api/main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+from tinydb import TinyDB, Query
+
+app = FastAPI()
+
+
+def memory_db():
+    db = TinyDB('db.json')
+    return db
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+def main():
+    uvicorn.run("bbs_api.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    main()
